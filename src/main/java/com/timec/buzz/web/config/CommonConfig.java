3a4d6e421386426c964906d7f2866ef96a548e6e
timec <timec.june@gmail.com>
2022-02-27 16:38:11 +0900

    spring.profile.active에 따른 망 분리 프로퍼티 추가

--- src/main/java/com/timec/buzz/web/config/CommonConfig.java
+++ src/main/java/com/timec/buzz/web/config/CommonConfig.java
@@ -1,7 +1,11 @@
 package com.timec.buzz.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "classpath:application-oauth.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:application-${spring.profiles.active}-db.properties", ignoreResourceNotFound = true)
 public class CommonConfig {
 }
